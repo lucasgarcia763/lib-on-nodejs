26b5b55200ad4237aae9caa982e1db2a3e8aab4e
Lucas Garcia <lucas.garcia763@live.com>
2019-10-21 14:17:06 -0300

    Definição de rotas sem express, tornando o projeto uma aplicação node, instalação do express e organização do projeto

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 const http = require('http');
 
 const server = http.createServer(function(request, response) {
-    response.end
-    (
-      `
-      <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1>LibOn - Your online library</h1>
-            </body>
-      </html>
-      `  
-    );
+    
+    let html = '';
+    
+    if(request.url == '/'){
+        html = `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1>LibOn - Your online library</h1>
+                </body>
+            </html>
+        `;
+    } else if(request.url == '/books'){
+        html = `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1>Books List</h1>
+                </body>
+            </html>
+        `;
+    }
+
+    response.end(html);
+
 });
 
 server.listen(3000);
