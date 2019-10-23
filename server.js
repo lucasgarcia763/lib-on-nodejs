3ca548b4f12542e11195ddbe4c35d3314c942089
Lucas Garcia <lucas.garcia763@live.com>
2019-10-23 12:30:55 -0300

    Implementação de rotas com o express e remoção da pasta node_modules

--- server.js
+++ server.js
@@ -1,35 +1,68 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer(function(request, response) {
-    
-    let html = '';
-    
-    if(request.url == '/'){
-        html = `
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1>LibOn - Your online library</h1>
-                </body>
-            </html>
-        `;
-    } else if(request.url == '/books'){
-        html = `
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1>Books List</h1>
-                </body>
-            </html>
-        `;
-    }
-
-    response.end(html);
-
+app.listen(3000, function(){
+    console.log(`Servidor rodando na porta 3000`);
 });
 
-server.listen(3000);
+app.get('/', function(request, response){
+    response.send(`
+                <html>
+                    <head>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1>LibOn - Your online library</h1>
+                    </body>
+                </html>
+            `);
+});
+
+app.get('/books', function(request, response){
+    response.send(`
+                <html>
+                    <head>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1>Books List</h1>
+                    </body>
+                </html>
+            `);
+});
+
+//const http = require('http');
+//
+//const server = http.createServer(function(request, response) {
+//    
+//    let html = '';
+//    
+//    if(request.url == '/'){
+//        html = `
+//            <html>
+//                <head>
+//                    <meta charset="utf-8">
+//                </head>
+//                <body>
+//                    <h1>LibOn - Your online library</h1>
+//                </body>
+//            </html>
+//        `;
+//    } else if(request.url == '/books'){
+//        html = `
+//            <html>
+//                <head>
+//                    <meta charset="utf-8">
+//                </head>
+//                <body>
+//                    <h1>Books List</h1>
+//                </body>
+//            </html>
+//        `;
+//    }
+//
+//    response.end(html);
+//
+//});
+//
+//server.listen(3000);
